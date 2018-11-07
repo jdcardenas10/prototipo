0354da871a63d3020dfa16466020ec14f13e333c
jdcardenas10 <jd.cardenas10@uniandes.edu.co>
2018-11-06 20:00:22 -0500

    descripciones

--- src/app/categoria/categoria.component.ts
+++ src/app/categoria/categoria.component.ts
@@ -8,10 +8,21 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CategoriaComponent implements OnInit {
 
   @Input() categoria: String;
+  subcategorias: String[];
+  esta: String;
+  salida: String="";
 
   constructor() { }
 
   ngOnInit() {
+  	this.subcategorias=this.categoria.split("/");
+  	this.esta=this.subcategorias[1];
+  	this.subcategorias.splice(0, 2);
+  	//this.subcategorias.splice(1, 1);
+  	for (let i in this.subcategorias) {
+   		this.salida=this.salida+"/"+this.subcategorias[i]
+	}
+
   }
 
 }
